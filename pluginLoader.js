e2a181b510caa4ed40fb3b1bb03cb951895818ae
Chris Troutner <chris.troutner@gmail.com>
2016-12-22 09:57:33 -0800

    Trying to load a view dynamically.

--- pluginLoader.js
+++ pluginLoader.js
@@ -1,11 +1,20 @@
 debugger;
 
+//Add this plugin to the loadedPlugins array.
+var thisPlugin = new Object();
+thisPlugin.views = [];
+thisPlugin.models = [];
+global.pluginView.loadedPlugins.push(thisPlugin);
 
+//Get the index of this plugin and store in the pluginData, for refrence from within the plugin's own code.
+var pluginIndex = global.pluginView.loadedPlugins.length-1;
+global.pluginView.pluginData[pluginIndex].pluginIndex = pluginIndex;
 
-//Get the JSON settings for this plugin.
+//Get a local copy of the JSON settings for this plugin.
 var pluginData = global.pluginView.pluginData[pluginIndex];
 var pluginDir = '/plugins/'+pluginData.pluginDirName+'/';
 
+debugger;
 
 //Load the individual views for this plugin.
 $.getScript(pluginDir+'backbone/views/exampleView1.js', function(data, textStatus, jqxhr) {
