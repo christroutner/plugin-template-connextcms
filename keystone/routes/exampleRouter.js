caa93d51438edb194fe21ca6f4f3f1c7f6fe3e2f
Chris Troutner <chris.troutner@gmail.com>
2016-12-24 09:34:03 -0800

    testing dynamic routing ideas

--- keystone/routes/exampleRouter.js
+++ keystone/routes/exampleRouter.js
@@ -8,10 +8,7 @@ var routes = {
 	api: importRoutes('./api') 
 };
 
-debugger;
-
 module.exports = function(app) {
-  debugger;
 
   app.get('/api/exampleplugin/list', keystone.middleware.api, routes.api.exampleplugin.list);
   app.all('/api/exampleplugin/create', keystone.middleware.api, routes.api.exampleplugin.create);
