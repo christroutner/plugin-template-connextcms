1243ffb877c19c3871eadd6586e43557c2a3164e
Chris Troutner <chris.troutner@gmail.com>
2017-01-06 13:29:36 -0800

    Refactoring for cleaner code

--- keystone/routes/exampleRouter.js
+++ keystone/routes/exampleRouter.js
@@ -10,9 +10,16 @@ var routes = {
 
 module.exports = function(app) {
 
+  // Keystone Views
+  app.get('/test', routes.views.test);
+  
+  // Plugin API Route
   app.get('/api/exampleplugin/list', keystone.middleware.api, routes.api.exampleplugin.list);
   app.all('/api/exampleplugin/create', keystone.middleware.api, routes.api.exampleplugin.create);
   app.all('/api/exampleplugin/:id/update', keystone.middleware.api, routes.api.exampleplugin.update);
 	app.get('/api/exampleplugin/:id/remove', keystone.middleware.api, routes.api.exampleplugin.remove);
   
+  // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+	// app.get('/protected', middleware.requireUser, routes.views.protected);
+  app.get('/loggedinuser', middleware.requireUser, routes.views.loggedinuser);
 }
