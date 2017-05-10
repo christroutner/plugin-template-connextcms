3504ec332d96a41f33c621c175d3b9946be655c7
Chris Troutner <chris.troutner@gmail.com>
2017-05-09 17:33:41 -0700

    Rewriting example plugin with new loader techniques

--- connextcms/views/exampleView1.js
+++ connextcms/views/exampleView1.js
@@ -32,7 +32,10 @@ var ExampleView1 = Backbone.View.extend({
   initialize: function () {
     debugger;
     
+    //Load the plugin metdata as a local variables.
     this.pluginData = this.options.pluginData;
+    
+    //Load a handle to the plugin constructs as a local variable.
     this.pluginHandle = this.options.pluginHandle;
     
     var thisView = this; //Maitain scope inside the AJAX handler.
